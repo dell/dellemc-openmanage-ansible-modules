7601c7155d2dd175b2b9aa752696a72bb192bf41
Abhishek Sinha <Abhishek.Sinha10@Dell.com>
2023-08-08 14:50:06 +0530

    Updated rst

--- plugins/modules/ome_alert_policies_info.py
+++ plugins/modules/ome_alert_policies_info.py
@@ -32,7 +32,7 @@ requirements:
 author: "Abhishek Sinha(@ABHISHEK-SINHA10)"
 notes:
     - Run this module from a system that has direct access to Dell OpenManage Enterprise.
-    - This module supports both IPv4 and IPv6 address.
+    - This module supports both IPv4 and IPv6 addresses.
     - This module supports C(check_mode).
 '''
 
