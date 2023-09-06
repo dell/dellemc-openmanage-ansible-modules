ae0ec5a6d0462c388f34f4a1fd323ecb2b352841
Jagadeesh N V <jagadeesh.n.v@dell.com>
2023-09-06 15:57:07 +0530

    UT for delete and enable cases

--- tests/unit/plugins/modules/test_ome_alert_policies.py
+++ tests/unit/plugins/modules/test_ome_alert_policies.py
@@ -23,7 +23,10 @@ from ansible_collections.dellemc.openmanage.plugins.modules import ome_alert_pol
 from ansible_collections.dellemc.openmanage.tests.unit.plugins.modules.common import FakeAnsibleModule
 
 MODULE_PATH = 'ansible_collections.dellemc.openmanage.plugins.modules.ome_alert_policies.'
-SUCCESS_MSG = "Successfully retrieved alert policies category information."
+
+SUCCESS_MSG = "Successfully performed the {0} operation."
+NO_CHANGES_MSG = "No changes found to be applied."
+CHANGES_MSG = "Changes found to be applied."
 
 
 @pytest.fixture
@@ -36,6 +39,69 @@ def ome_connection_mock_for_alert_policies(mocker, ome_response_mock):
 
 class TestOmeAlertPolicies(FakeAnsibleModule):
     module = ome_alert_policies
+
+    @pytest.mark.parametrize("params", [
+        {"message": SUCCESS_MSG.format("toggle enable policy"), "success": True,
+         "json_data": {"value": [{'Name': "new alert policy", "Id": 12, "Enabled": False}]},
+         "mparams": {"name": "new alert policy", "enable": True}},
+        {"message": CHANGES_MSG, "success": True, "check_mode": True,
+         "json_data": {"value": [{'Name': "new alert policy", "Id": 12, "Enabled": False}]},
+         "mparams": {"name": "new alert policy", "enable": True}},
+        {"message": "More than one policy name provided for update.", "success": True,
+         "json_data": {"value": [{'Name': "alert policy1", "Id": 12, "Enabled": True},
+                                 {'Name': "alert policy2", "Id": 13, "Enabled": True}]},
+         "mparams": {"name": ["alert policy1", "alert policy2"], "enable": False, "description": 'Update case failed'}},
+        {"message": "Policies alert policy3 are invalid for enable.", "success": True,
+         "json_data": {"value": [{'Name': "alert policy1", "Id": 12, "Enabled": True},
+                                 {'Name': "alert policy2", "Id": 13, "Enabled": True}]},
+         "mparams": {"name": ["alert policy3", "alert policy2"], "enable": False}},
+        {"message": NO_CHANGES_MSG, "success": True, "check_mode": True,
+         "json_data": {"value": [{'Name': "new alert policy", "Id": 12, "Enabled": False}]},
+         "mparams": {"name": "new alert policy", "enable": False}}
+    ])
+    def test_ome_alert_policies_enable_disable(self, params, ome_connection_mock_for_alert_policies,
+                                               ome_response_mock, ome_default_args, module_mock, mocker):
+        ome_response_mock.success = params.get("success", True)
+        ome_response_mock.json_data = params['json_data']
+        ome_connection_mock_for_alert_policies.get_all_items_with_pagination.return_value = params[
+            'json_data']
+        ome_default_args.update(params['mparams'])
+        result = self._run_module(
+            ome_default_args, check_mode=params.get('check_mode', False))
+        assert result['msg'] == params['message']
+
+    @pytest.mark.parametrize("params", [
+        {"message": SUCCESS_MSG.format("remove policy"), "success": True,
+         "json_data": {"report_list": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}],
+                       "value": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}]},
+         "mparams": {"name": "new alert policy", "state": "absent"}},
+        {"message": CHANGES_MSG, "success": True, "check_mode": True,
+         "json_data": {"report_list": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}],
+                       "value": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}]},
+         "mparams": {"name": "new alert policy", "state": "absent"}},
+        {"message": "Default Policies new alert policy cannot be deleted.", "success": True,
+         "json_data": {"report_list": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}],
+                       "value": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": True}]},
+         "mparams": {"name": "new alert policy", "state": "absent"}},
+        {"message": NO_CHANGES_MSG, "success": True, "check_mode": True,
+         "json_data": {"report_list": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}],
+                       "value": [{'Name': "new alert policy 1", "Id": 12, "DefaultPolicy": False}]},
+         "mparams": {"name": "new alert policy", "state": "absent"}},
+        {"message": NO_CHANGES_MSG, "success": True,
+         "json_data": {"report_list": [{'Name': "new alert policy", "Id": 12, "DefaultPolicy": False}],
+                       "value": [{'Name': "new alert policy 1", "Id": 12, "DefaultPolicy": False}]},
+         "mparams": {"name": "new alert policy", "state": "absent"}},
+    ])
+    def test_ome_alert_policies_delete(self, params, ome_connection_mock_for_alert_policies,
+                                       ome_response_mock, ome_default_args, module_mock, mocker):
+        ome_response_mock.success = params.get("success", True)
+        ome_response_mock.json_data = params['json_data']
+        ome_connection_mock_for_alert_policies.get_all_items_with_pagination.return_value = params[
+            'json_data']
+        ome_default_args.update(params['mparams'])
+        result = self._run_module(
+            ome_default_args, check_mode=params.get('check_mode', False))
+        assert result['msg'] == params['message']
 
     @pytest.mark.parametrize("exc_type",
                              [SSLValidationError, ConnectionError, TypeError, ValueError, OSError, HTTPError, URLError])
