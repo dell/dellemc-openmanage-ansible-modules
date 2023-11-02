446e8fedc9cc6d09336b56dfadb58e2753d40235
Sachin Apagundi <Sachin_Apagundi@dell.com>
2023-11-02 16:12:43 +0530

    fix sanity

--- plugins/inventory/ome_inventory.py
+++ plugins/inventory/ome_inventory.py
@@ -173,10 +173,10 @@ class InventoryModule(BaseInventoryPlugin):
         port = self.get_option("port") if "port" in self.config else 443
         validate_certs = self.get_option("validate_certs") if "validate_certs" in self.config else False
         module_params = {
-            "hostname": self.get_option("hostname"), 
+            "hostname": self.get_option("hostname"),
             "username": self.get_option("username"),
-            "password": self.get_option("password"), 
-            "port": port, 
+            "password": self.get_option("password"),
+            "port": port,
             "validate_certs": validate_certs}
         if "ca_path" in self.config:
             module_params.update({"ca_path": self.get_option("ca_path")})
