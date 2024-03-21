df741061bc6ae879ecc22a7d66c643a5a3a43cf7
Sachin Apagundi <62133262+sachin-apa@users.noreply.github.com>
2024-03-21 14:29:14 +0530

    Fix the static code check issues (#633)
    * updated hotspots
    * updated duplicate lines

--- tests/unit/plugins/modules/test_ome_device_power_settings.py
+++ tests/unit/plugins/modules/test_ome_device_power_settings.py
@@ -35,6 +35,28 @@ POWER_FAIL_MSG = "Unable to complete the operation because the power settings " 
                  "are not supported on the specified device."
 DOMAIN_FAIL_MSG = "The device location settings operation is supported only on " \
                   "OpenManage Enterprise Modular."
+ERROR_JSON = {
+    "error": {
+        "code": "Base.1.0.GeneralError",
+        "message": "A general error has occurred. See ExtendedInfo for more information.",
+        "@Message.ExtendedInfo": [
+            {
+                "MessageId": "CGEN1004",
+                "RelatedProperties": [],
+                "Message": "Unable to process the request because an error occurred.",
+                "MessageArgs": [],
+                "Severity": "Critical",
+                            "Resolution": "Retry the operation. If the issue persists, contact your system administrator."
+            }
+        ]
+    }}
+MPARAMS = {"hostname": "xxx.xxx.x.x",
+           "power_configuration": {"enable_power_cap": True, "power_cap": 3424}
+           }
+POWER_JSON_DATA = {"value": [
+    {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
+     'DeviceId': 1234, "Type": 1000},
+    {'PublicAddress': "xxx.xxx.xx.x", 'DeviceId': 1235, "Type": 1000}]}
 
 
 @pytest.fixture
@@ -49,9 +71,9 @@ class TestOMEMDevicePower(FakeAnsibleModule):
 
     module = ome_device_power_settings
 
-    @pytest.mark.parametrize("params", [
+    @pytest.mark.parametrize("params_inp", [
         {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
+            {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
              'DeviceServiceTag': 'ABCD123', "Type": 1000},
             {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}],
             "EnableHotSpare": True,
@@ -63,13 +85,13 @@ class TestOMEMDevicePower(FakeAnsibleModule):
             "RedundancyPolicy": "NO_REDUNDANCY",
             "SettingType": "Power"},
             'message': SUCCESS_MSG,
-            'mparams': {"hostname": "1.2.3.4",
+            'mparams': {"hostname": "xxx.xxx.x.x",
                         "power_configuration": {"enable_power_cap": True, "power_cap": 3424},
                         "hot_spare_configuration": {"enable_hot_spare": False, "primary_grid": "GRID_1"},
                         "device_id": 1234,
                         }},
         {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
+            {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
              'DeviceServiceTag': 'ABCD123', "Type": 1000},
             {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}],
             "EnableHotSpare": True,
@@ -81,13 +103,13 @@ class TestOMEMDevicePower(FakeAnsibleModule):
             "RedundancyPolicy": "NO_REDUNDANCY",
             "SettingType": "Power"},
             'message': SUCCESS_MSG,
-            'mparams': {"hostname": "1.2.3.4",
+            'mparams': {"hostname": "xxx.xxx.x.x",
                         "power_configuration": {"enable_power_cap": False, "power_cap": 3424},
                         "hot_spare_configuration": {"enable_hot_spare": True, "primary_grid": "GRID_1"},
                         "device_service_tag": 'ABCD123',
                         }},
         {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
+            {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
              'DeviceId': 1234, "Type": 1000},
             {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}],
             "EnableHotSpare": True,
@@ -99,7 +121,7 @@ class TestOMEMDevicePower(FakeAnsibleModule):
             "RedundancyPolicy": "NO_REDUNDANCY",
             "SettingType": "Power"},
             'message': SUCCESS_MSG,
-            'mparams': {"hostname": "1.2.3.4",
+            'mparams': {"hostname": "xxx.xxx.x.x",
                         "power_configuration": {"enable_power_cap": False, "power_cap": 3424},
                         "hot_spare_configuration": {"enable_hot_spare": True, "primary_grid": "GRID_1"}
                         }},
@@ -121,20 +143,17 @@ class TestOMEMDevicePower(FakeAnsibleModule):
                         "hot_spare_configuration": {"enable_hot_spare": True, "primary_grid": "GRID_1"}
                         }}
     ])
-    def test_ome_devices_power_settings_success(self, params, ome_conn_mock_power, ome_response_mock,
+    def test_ome_devices_power_settings_success(self, params_inp, ome_conn_mock_power, ome_response_mock,
                                                 ome_default_args, module_mock, mocker):
-        ome_response_mock.success = params.get("success", True)
-        ome_response_mock.json_data = params['json_data']
-        ome_default_args.update(params['mparams'])
+        ome_response_mock.success = params_inp.get("success", True)
+        ome_response_mock.json_data = params_inp['json_data']
+        ome_default_args.update(params_inp['mparams'])
         result = self._run_module(
-            ome_default_args, check_mode=params.get('check_mode', False))
-        assert result['msg'] == params['message']
+            ome_default_args, check_mode=params_inp.get('check_mode', False))
+        assert result['msg'] == params_inp['message']
 
     @pytest.mark.parametrize("params", [
-        {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
-             'DeviceId': 1234, "Type": 1000},
-            {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}]},
+        {"json_data": POWER_JSON_DATA,
             'message': DOMAIN_FAIL_MSG,
             'http_error_json': {
                 "error": {
@@ -151,77 +170,39 @@ class TestOMEMDevicePower(FakeAnsibleModule):
                         }
                     ]
                 }},
-            'mparams': {"hostname": "1.2.3.4",
+            'mparams': {"hostname": "xxx.xxx.x.x",
                         "device_service_tag": 'ABCD123',
                         "power_configuration": {"enable_power_cap": True, "power_cap": 3424}
                         }},
         {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
+            {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
              'DeviceId': 1234, "Type": 1000},
             {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}]},
             'message': POWER_FAIL_MSG,
             'check_domain_service': 'mocked_check_domain_service',
             'get_chassis_device': ('Id', 1234),
-            'http_error_json': {
-                "error": {
-                    "code": "Base.1.0.GeneralError",
-                    "message": "A general error has occurred. See ExtendedInfo for more information.",
-                    "@Message.ExtendedInfo": [
-                        {
-                            "MessageId": "CGEN1004",
-                            "RelatedProperties": [],
-                            "Message": "Unable to process the request because an error occurred.",
-                            "MessageArgs": [],
-                            "Severity": "Critical",
-                            "Resolution": "Retry the operation. If the issue persists, contact your system administrator."
-                        }
-                    ]
-                }},
-            'mparams': {"hostname": "1.2.3.4",
-                        "power_configuration": {"enable_power_cap": True, "power_cap": 3424}
-                        }},
-        {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
-             'DeviceId': 1234, "Type": 1000},
-            {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}]},
+            'http_error_json': ERROR_JSON,
+            'mparams': MPARAMS},
+        {"json_data": POWER_JSON_DATA,
             'message': POWER_FAIL_MSG,
             'check_domain_service': 'mocked_check_domain_service',
             'get_chassis_device': ('Id', 1234),
             'http_err_code': 404,
-            'http_error_json': {
-                "error": {
-                    "code": "Base.1.0.GeneralError",
-                    "message": "A general error has occurred. See ExtendedInfo for more information.",
-                    "@Message.ExtendedInfo": [
-                        {
-                            "MessageId": "CGEN1004",
-                            "RelatedProperties": [],
-                            "Message": "Unable to process the request because an error occurred.",
-                            "MessageArgs": [],
-                            "Severity": "Critical",
-                            "Resolution": "Retry the operation. If the issue persists, contact your system administrator."
-                        }
-                    ]
-                }},
-            'mparams': {"hostname": "1.2.3.4",
-                        "power_configuration": {"enable_power_cap": True, "power_cap": 3424}
-                        }},
-        {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
-             'DeviceId': 1234, "Type": 1000},
-            {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}]},
+            'http_error_json': ERROR_JSON,
+            'mparams': MPARAMS},
+        {"json_data": POWER_JSON_DATA,
             'message': DEVICE_FAIL_MSG.format('id', 123),
             'check_domain_service': 'mocked_check_domain_service',
             'get_chassis_device': ('Id', 1234),
-            'mparams': {"hostname": "1.2.3.4", 'device_id': 123,
+            'mparams': {"hostname": "xxx.xxx.x.x", 'device_id': 123,
                         "power_configuration": {"enable_power_cap": True, "power_cap": 3424}
                         }},
         {"json_data": {"value": [
-            {'Id': 1234, 'PublicAddress': "1.2.3.4",
+            {'Id': 1234, 'PublicAddress': "xxx.xxx.x.x",
              'DeviceId': 1234, "Type": 1000},
             {'PublicAddress': "1.2.3.5", 'DeviceId': 1235, "Type": 1000}]},
             'message': CONFIG_FAIL_MSG,
-            'mparams': {"hostname": "1.2.3.4", "device_id": 123}}
+            'mparams': {"hostname": "xxx.xxx.x.x", "device_id": 123}}
     ])
     def test_ome_devices_power_settings_failure(self, params, ome_conn_mock_power, ome_response_mock,
                                                 ome_default_args, module_mock, mocker):
@@ -303,29 +284,29 @@ class TestOMEMDevicePower(FakeAnsibleModule):
         result = self.module.get_ip_from_host("ZZ.ZZ.ZZ.ZZ")
         assert result == "ZZ.ZZ.ZZ.ZZ"
 
-    @pytest.mark.parametrize("exc_type",
+    @pytest.mark.parametrize("exc_type_ps",
                              [IOError, ValueError, SSLError, TypeError, ConnectionError, HTTPError, URLError])
-    def test_ome_device_power_main_exception_case(self, exc_type, mocker, ome_default_args,
+    def test_ome_device_power_main_exception_case(self, exc_type_ps, mocker, ome_default_args,
                                                   ome_conn_mock_power, ome_response_mock):
         ome_default_args.update({"device_id": 25011, "power_configuration": {"enable_power_cap": True,
                                                                              "power_cap": 3424}})
         ome_response_mock.status_code = 400
         ome_response_mock.success = False
         json_str = to_text(json.dumps({"info": "error_details"}))
-        if exc_type == URLError:
+        if exc_type_ps == URLError:
             mocker.patch(MODULE_PATH + 'check_domain_service',
-                         side_effect=exc_type("url open error"))
-            result = self._run_module(ome_default_args)
-            assert result["unreachable"] is True
-        elif exc_type not in [HTTPError, SSLValidationError]:
+                         side_effect=exc_type_ps("url open error"))
+            result_ps = self._run_module(ome_default_args)
+            assert result_ps["unreachable"] is True
+        elif exc_type_ps not in [HTTPError, SSLValidationError]:
             mocker.patch(MODULE_PATH + 'check_domain_service',
-                         side_effect=exc_type("exception message"))
-            result = self._run_module_with_fail_json(ome_default_args)
-            assert result['failed'] is True
+                         side_effect=exc_type_ps("exception message"))
+            result_ps = self._run_module_with_fail_json(ome_default_args)
+            assert result_ps['failed'] is True
         else:
             mocker.patch(MODULE_PATH + 'check_domain_service',
-                         side_effect=exc_type('https://testhost.com', 400, 'http error message',
-                                              {"accept-type": "application/json"}, StringIO(json_str)))
-            result = self._run_module_with_fail_json(ome_default_args)
-            assert result['failed'] is True
-        assert 'msg' in result
+                         side_effect=exc_type_ps('https://testhost.com', 400, 'http error message',
+                                                 {"accept-type": "application/json"}, StringIO(json_str)))
+            result_ps = self._run_module_with_fail_json(ome_default_args)
+            assert result_ps['failed'] is True
+        assert 'msg' in result_ps
