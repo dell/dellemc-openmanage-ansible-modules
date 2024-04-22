e63f9e346844a948467e952768796455ab697d6d
Lovepreet Singh <Lovepreet.Singh1@dell.com>
2024-04-22 08:04:18 -0400

    Minor Changes
    Signed-off-by: Lovepreet Singh <Lovepreet.Singh1@dell.com>

--- tests/unit/plugins/modules/test_idrac_reset.py
+++ tests/unit/plugins/modules/test_idrac_reset.py
@@ -3,7 +3,7 @@
 #
 # Dell OpenManage Ansible Modules
 # Version 9.2.0
-# Copyright (C) 2020-24 Dell Inc. or its subsidiaries. All Rights Reserved.
+# Copyright (C) 2020-2024 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
