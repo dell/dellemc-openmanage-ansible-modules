066b09d7e2dff747374243099c528f0314495cec
Kritika-Bhateja-03 <Kritika.Bhateja@Dell.com>
2024-05-23 07:02:09 +0000

    Sanity fixed

--- plugins/module_utils/session_utils.py
+++ plugins/module_utils/session_utils.py
@@ -34,8 +34,7 @@ import os
 from ansible.module_utils.urls import open_url
 from ansible.module_utils.six.moves.urllib.parse import urlencode
 from ansible_collections.dellemc.openmanage.plugins.module_utils.utils import config_ipv6
-from ansible.module_utils.urls import open_url, ConnectionError, SSLValidationError
-from ansible.module_utils.six.moves.urllib.error import URLError, HTTPError
+from ansible.module_utils.urls import open_url
 from abc import ABC, abstractmethod
 
 HEADER_TYPE = "application/json"
