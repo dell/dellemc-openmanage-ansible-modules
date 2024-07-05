14888611e3028c8a3d2ed4105abee86a79c4448c
ABHISHEK-SINHA10 <Abhishek.Sinha10@Dell.com>
2024-07-05 11:27:01 +0530

    Minor update

--- plugins/modules/idrac_redfish_storage_controller.py
+++ plugins/modules/idrac_redfish_storage_controller.py
@@ -891,14 +891,17 @@ def validate_secure_erase(module, redfish_obj):
     if LooseVersion(firm_ver) >= '3.0':
         dell_oem = drive_detail.get("Oem", {}).get("Dell", {})
         try:
-            dell_disk = dell_oem.get("DellPhysicalDisk", {})
+            dell_disk = dell_oem["DellPhysicalDisk"]
         except Exception:
             dell_disk = dell_oem.get("DellPCIeSSD", {})
         drive_ready = dell_disk.get("RaidStatus", {})
-        capable = dell_disk.get("SystemEraseCapability", {})
         if drive_ready != "Ready":
-            module.exit_json(msg=DRIVE_NOT_READY.format(drive_id), skipped=True)
-        if capable != "CryptographicErasePD":
+            module.exit_json(msg=DRIVE_NOT_READY.format(drive_id),
+                             skipped=True)
+        drive_detail = dell_disk
+
+    capable = drive_detail.get("SystemEraseCapability", {})
+    if capable != "CryptographicErasePD":
             module.exit_json(msg=DRIVE_NOT_SECURE_ERASE.format(drive_id),
                              skipped=True)
     return drive_uri, job_type
