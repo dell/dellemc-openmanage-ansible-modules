dbc8e18f70cea159af3ffb167cf6efe8ca8817a7
Saksham Nautiyal <saksham.nautiyal@dell.com>
2024-10-16 18:42:49 +0530

    [idrac_firmware_info] - Enhancement to support iDRAC 17G

--- plugins/modules/idrac_firmware_info.py
+++ plugins/modules/idrac_firmware_info.py
@@ -3,8 +3,8 @@
 
 #
 # Dell OpenManage Ansible Modules
-# Version 7.1.0
-# Copyright (C) 2021-2022 Dell Inc. or its subsidiaries. All Rights Reserved.
+# Version 9.8.0
+# Copyright (C) 2021-2024 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
@@ -25,7 +25,9 @@ extends_documentation_fragment:
 requirements:
     - "omsdk >= 1.2.488"
     - "python >= 3.9.6"
-author: "Rajeev Arakkal (@rajeevarakkal)"
+author:
+  - "Rajeev Arakkal (@rajeevarakkal)"
+  - "Saksham Nautiyal (@Saksham-Nautiyal)"
 notes:
     - Run this module from a system that has direct access to Dell iDRAC.
     - This module supports both IPv4 and IPv6 address for I(idrac_ip).
@@ -104,34 +106,60 @@ error_info:
   }
 '''
 
-
 import json
 from ansible_collections.dellemc.openmanage.plugins.module_utils.dellemc_idrac import iDRACConnection, idrac_auth_params
+from ansible_collections.dellemc.openmanage.plugins.module_utils.idrac_redfish import iDRACRedfishAPI
+from ansible_collections.dellemc.openmanage.plugins.module_utils.utils import GET_IDRAC_FIRMWARE_DETAILS_URI_10, GET_IDRAC_FIRMWARE_URI_10, remove_key
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.urls import ConnectionError, SSLValidationError
-from ansible.module_utils.six.moves.urllib.error import URLError, HTTPError
+from urllib.error import URLError, HTTPError
+
+ERR_STATUS = 404
 
 
-# Main
+def get_from_wsman(module):
+    with iDRACConnection(module.params) as idrac:
+        firmware_details = idrac.update_mgr.InstalledFirmware
+        return firmware_details
+
+
+def get_idrac_firmware_info(idrac, module):
+    try:
+        response = idrac.invoke_request(method='GET', uri=GET_IDRAC_FIRMWARE_URI_10)
+
+        if response.status_code == 200:
+            details_response = idrac.invoke_request(method='GET', uri=GET_IDRAC_FIRMWARE_DETAILS_URI_10)
+
+            if details_response and details_response.status_code == 200 and details_response.json_data:
+                filtered_data = remove_key(details_response.json_data)
+                return filtered_data
+
+    except HTTPError as err:
+        if err.status == 404:
+            return get_from_wsman(module)
+
+
 def main():
     specs = {}
     specs.update(idrac_auth_params)
     module = AnsibleModule(
         argument_spec=specs,
-        supports_check_mode=True)
+        supports_check_mode=True
+    )
 
     try:
-        with iDRACConnection(module.params) as idrac:
-            msg = idrac.update_mgr.InstalledFirmware
+        with iDRACRedfishAPI(module.params) as idrac:
+            firmware_info = get_idrac_firmware_info(idrac, module)
     except HTTPError as err:
-        module.fail_json(msg=str(err), error_info=json.load(err))
+        module.exit_json(msg=str(err), error_info=json.load(err), failed=True)
     except URLError as err:
         module.exit_json(msg=str(err), unreachable=True)
     except (RuntimeError, SSLValidationError, IOError, ValueError, TypeError, ConnectionError) as e:
-        module.fail_json(msg=str(e))
-
-    module.exit_json(msg="Successfully fetched the firmware inventory details.",
-                     firmware_info=msg)
+        module.exit_json(msg=str(e), failed=True)
+    module.exit_json(
+        msg="Successfully fetched the firmware inventory details.",
+        firmware_info=firmware_info
+    )
 
 
 if __name__ == '__main__':
