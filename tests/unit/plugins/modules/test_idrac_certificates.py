9dd787d37b45a08beba544394a63082b6305a7be
ABHISHEK-SINHA10 <Abhishek.Sinha10@Dell.com>
2024-12-12 10:41:55 +0530

    Updated version and copyright year

--- tests/unit/plugins/modules/test_idrac_certificates.py
+++ tests/unit/plugins/modules/test_idrac_certificates.py
@@ -2,8 +2,8 @@
 
 #
 # Dell OpenManage Ansible Modules
-# Version 8.6.0
-# Copyright (C) 2022-2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+# Version 9.10.0
+# Copyright (C) 2022-2024 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
