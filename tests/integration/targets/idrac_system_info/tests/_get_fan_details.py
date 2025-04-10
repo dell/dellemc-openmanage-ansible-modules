f91be0060e1e62bb2a859afc698fc70b59a2f690
Kritika Bhateja <Kritika.Bhateja@Dell.com>
2025-04-10 19:07:51 +0530

    Added integration

--- tests/integration/targets/idrac_system_info/tests/_get_fan_details.py
+++ tests/integration/targets/idrac_system_info/tests/_get_fan_details.py
@@ -5,38 +5,25 @@ import ast
 fan_api_output = sys.argv[1]
 NA = "Not Available"
 
+
 def map_fan_data(fan):
     health = fan.get("Status", {}).get("Health", NA)
     fan_pwm = fan.get("Oem", {}).get("Dell", {}).get("FanPWM", 0)
-    keys_to_search = {
-        "ActiveCooling": "HotPluggable",
-        "CurrentReading": "SpeedPercent.SpeedRPM",
-        "DeviceDescription": "Name",
-        "FQDD": "Id",
-        "Key": "Id",
-        "Location": "Location",
-        "PWM": "Oem.Dell.FanPWM",
-        "PrimaryStatus": "Status.Health",
-        "State": "State",
+    current_reading = fan.get("SpeedPercent", {}).get("SpeedRPM", NA)
+    output = {
+        "ActiveCooling": fan.get("HotPluggable", NA),
+        "CurrentReading": current_reading,
+        "DeviceDescription": fan.get("Name", NA),
+        "FQDD": fan.get("Id", NA),
+        "Key": fan.get("Id", NA),
+        "Location": fan.get("Location", NA),
+        "PWM": fan_pwm,
+        "PrimaryStatus": "Healthy" if health == "OK" else health,
+        "State": fan.get("State", NA),
         "VariableSpeed": "true" if fan_pwm > 0 else "false"
     }
+    return output
 
-    fan_data = {}
-
-    for key, response_key in keys_to_search.items():
-        if key == "PrimaryStatus":
-            fan_data[key] = "Healthy" if health == "OK" else (health or NA)
-        elif key == "VariableSpeed":
-            fan_pwm = fan.get("Oem", {}).get("Dell", {}).get("FanPWM", 0)
-            fan_data[key] = "true" if fan_pwm > 0 else "false"
-        else:
-            keys = response_key.split(".")
-            value = fan
-            for k in keys:
-                value = value.get(k, NA) if isinstance(value, dict) else NA
-            fan_data[key] = value
-
-    return fan_data
 
 output = []
 fan_output = ast.literal_eval(fan_api_output)
@@ -44,4 +31,4 @@ fan_output = json.loads(fan_output)
 fan_members = fan_output.get("Members", [])
 for fan in fan_members:
     output.append(map_fan_data(fan))
-print(output)
+print(json.dumps(output, ensure_ascii=False))
