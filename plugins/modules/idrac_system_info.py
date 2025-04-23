46581291f6d049adc985b9021ff80d5e6c64d09c
ABHISHEK-SINHA10 <Abhishek.Sinha10@dell.com>
2025-04-23 06:40:10 -0400

    Update module version

--- plugins/modules/idrac_system_info.py
+++ plugins/modules/idrac_system_info.py
@@ -3,7 +3,7 @@
 
 #
 # Dell OpenManage Ansible Modules
-# Version 7.1.0
+# Version 9.12.0
 # Copyright (C) 2021-2025 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
