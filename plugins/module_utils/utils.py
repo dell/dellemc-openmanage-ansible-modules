2795b8a85cd7e62c70f4d953cad98f9498441ed2
Kritika Bhateja <Kritika.Bhateja@Dell.com>
2025-05-13 10:50:32 +0530

    Updated utils

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -785,13 +785,15 @@ def validate_time(time, module):
 
 def get_job_uri_id(rest_obj):
     firmware_obj = IDRACFirmwareInfo(rest_obj)
+    job_uri_id = MANAGER_JOB_ID_URI
     if not firmware_obj.is_omsdk_required():
-        MANAGER_JOB_ID_URI = MANAGER_JOB_ID_URI_10
-    return MANAGER_JOB_ID_URI
+        job_uri_id = MANAGER_JOB_ID_URI_10
+    return job_uri_id
 
 
 def get_job_uri(rest_obj):
     firmware_obj = IDRACFirmwareInfo(rest_obj)
+    job_uri = MANAGER_JOB_URI
     if not firmware_obj.is_omsdk_required():
-        MANAGER_JOB_URI = MANAGER_JOB_URI_10
-    return MANAGER_JOB_URI
+        job_uri = MANAGER_JOB_URI_10
+    return job_uri
