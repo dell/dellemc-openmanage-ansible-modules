3513a7641a8aa273c7761c391b67a2d4152eb451
Saksham-Nautiyal <saksham.nautiyal@dell.com>
2025-05-26 12:13:58 +0530

    version changes in all module files

--- plugins/modules/redfish_powerstate.py
+++ plugins/modules/redfish_powerstate.py
@@ -4,7 +4,7 @@
 #
 # Dell OpenManage Ansible Modules
 # Version 9.5.0
-# Copyright (C) 2020-2024 Dell Inc. or its subsidiaries. All Rights Reserved.
+# Copyright (C) 2020-2025 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 #
