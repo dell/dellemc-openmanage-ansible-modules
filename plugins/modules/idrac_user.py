5f7c13d8d36725607984c59994ce6d857fb071ae
Kritika Bhateja <Kritika.Bhateja@Dell.com>
2025-07-07 17:30:15 +0530

    Updated message

--- plugins/modules/idrac_user.py
+++ plugins/modules/idrac_user.py
@@ -229,7 +229,7 @@ ManagerAttributeRegistry/ManagerAttributeRegistry.v1_0_0.json"
 USER_ROLES = {"Administrator": 511, "Operator": 499, "ReadOnly": 1, "None": 0}
 ACCESS = {0: "Disabled", 1: "Enabled"}
 INVALID_PRIVILAGE_MSG = "custom_privilege value must range from 0 to 511."
-INVALID_PRIVILAGE_MSG_NONE = "None is not an applicable value."
+INVALID_PRIVILAGE_MSG_NONE = "None is not an applicable value for privilege in iDRAC 17G and later."
 INVALID_PRIVILAGE_MIN_iDRAC9 = 0
 INVALID_PRIVILAGE_MIN_iDRAC10 = 1
 INVALID_PRIVILAGE_MAX = 511
