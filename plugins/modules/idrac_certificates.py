ee0946cf9ea8e6c7fccac6b648edbcac3e8c29cf
Datta <trisha_datta@dell.com>
2025-07-08 10:43:17 +0530

    Fixing lint and sanity issue

--- plugins/modules/idrac_certificates.py
+++ plugins/modules/idrac_certificates.py
@@ -111,7 +111,7 @@ options:
       - Maximum wait time for iDRAC to start after the reset, in seconds.
       - This is applicable when I(command) is C(import) or C(reset) and I(reset) is C(true).
     type: int
-    default: 300
+    default: 600
 requirements:
   - "python >= 3.9.6"
 author:
