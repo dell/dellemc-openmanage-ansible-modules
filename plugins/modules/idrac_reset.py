7c76381463bbe941cea90eb0100246c10df13646
Rounak Adhikary <Rounak.Adhikary@dell.com>
2025-07-10 11:01:39 -0400

    Versions

--- plugins/modules/idrac_reset.py
+++ plugins/modules/idrac_reset.py
@@ -3,7 +3,7 @@
 
 #
 # Dell OpenManage Ansible Modules
-# Version 9.12.1
+# Version 9.12.3
 # Copyright (C) 2018-2025 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
@@ -91,6 +91,7 @@ author:
   - "Anooja Vardhineni (@anooja-vardhineni)"
   - "Lovepreet Singh (@singh-lovepreet1)"
   - "Abhishek Sinha (@ABHISHEK-SINHA10)"
+  - "Rounak Adhikary (@rounak-adhikary)"
 notes:
     - Run this module from a system that has direct access to Dell iDRAC.
     - This module supports both IPv4 and IPv6 address for I(idrac_ip).
