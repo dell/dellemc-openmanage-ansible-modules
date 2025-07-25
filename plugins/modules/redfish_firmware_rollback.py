2b1b65475e18da6a1326b3119f67e813b2807aad
Sakshi_Makkar <Sakshi_Makkar@dell.com>
2025-07-25 03:49:14 -0400

    sanity and sonarqube issues resolved

--- plugins/modules/redfish_firmware_rollback.py
+++ plugins/modules/redfish_firmware_rollback.py
@@ -335,7 +335,7 @@ def rollback_firmware(redfish_obj, module, preview_uri, reboot_uri, update_uri):
     if reboot_uri:
         generation = redfish_obj.get_server_generation[0]
         job_ids = simple_update(redfish_obj, reboot_uri, update_uri)
-        track, resetting, _ = wait_for_redfish_idrac_reset(module, redfish_obj, 900, generation)
+        track, resetting = wait_for_redfish_idrac_reset(module, redfish_obj, 900, generation)
         if not track and resetting:
             reboot_job_status, failed = get_job_status(redfish_obj, module, job_ids, job_wait=True)
             current_job_status.extend(reboot_job_status)
