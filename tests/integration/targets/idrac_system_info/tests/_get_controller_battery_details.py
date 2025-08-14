e8440c6babd81ae9d63a8d6851c2e00be04d2264
Akshay Sripriya <akshay.sripriya@dell.com>
2025-08-14 00:46:47 -0500

    Adding Unit and Integration Tests
    Signed-off-by: akshay-sriprya <akshay.sripriya@dell.com>

---
+++ tests/integration/targets/idrac_system_info/tests/_get_controller_battery_details.py
@@ -0,0 +1,33 @@
+import json
+import sys
+
+NA = "Not Available"
+controller_battery_api_output = sys.argv[1]
+
+
+def map_controller_battery_data(battery):
+    health_state_map = {
+        "CriticalFailure": "Critical",
+        "Degraded/Warning": "Warning",
+        "MajorFailure": "Critical",
+        "MinorFailure": "Critical",
+        "NonRecoverableError": "Critical",
+        "OK": "Healthy",
+        "Unknown": "Unknown"
+    }
+    
+    health_state = battery.get("PrimaryStatus", NA)
+    primary_status = health_state_map.get(health_state, NA)
+    output = {
+        "DeviceDescription": battery.get("Name", NA),
+        "FQDD": battery.get("FQDD", NA),
+        "InstanceID": battery.get("Id", NA),
+        "Key": battery.get("Id", NA),
+        "PrimaryStatus": primary_status,
+        "RAIDState": battery.get("RAIDState", NA),
+    }
+    return output
+
+controller_battery_output = json.loads(controller_battery_api_output)
+output = [map_controller_battery_data(controller_battery) for controller_battery in controller_battery_output.get("Members", [])]
+print(output)
---
+++ tests/integration/targets/idrac_system_info/tests/_get_sensor_amperage_details.py
@@ -0,0 +1,37 @@
+import json
+import sys
+
+NA = "Not Available"
+sensor_amperage_api_output = sys.argv[1]
+
+def map_sensor_amperage_data(sensor):
+    health_state_map = {
+        "CriticalFailure": "Critical",
+        "Degraded/Warning": "Warning",
+        "MajorFailure": "Critical",
+        "MinorFailure": "Critical",
+        "NonRecoverableError": "Critical",
+        "OK": "Healthy",
+        "Unknown": "Unknown"
+    }
+    
+    health_state = sensor.get("Status", {}).get("Health", NA)
+    primary_status = health_state_map.get(health_state, NA)
+    output = {
+        "CurrentReading": sensor.get("Reading", NA),
+        "CurrentState": sensor.get("Oem", {}).get("Dell", {}).get("CurrentState", NA),
+        "DeviceID": sensor.get("Oem", {}).get("Dell", {}).get("DeviceID", NA),
+        "HealthState": sensor.get("Oem", {}).get("Dell", {}).get("CurrentState", NA),
+        "Key": sensor.get("Name", NA),
+        "Location": sensor.get("Name", NA),
+        "OtherSensorTypeDescription": NA,
+        "PrimaryStatus": primary_status,
+        "ProbeType": NA,
+        "SensorType": "Amperage",
+        "State": sensor.get("Status", {}).get("State", NA),
+    }
+    return output
+
+sensor_amperage_output = json.loads(sensor_amperage_api_output)
+output = [map_sensor_amperage_data(sensor_amperage) for sensor_amperage in sensor_amperage_output.get("Members", [])]
+print(output)
