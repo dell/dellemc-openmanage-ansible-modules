0a7566fb9083d5556a1b00eb26138ca1d25dfa59
Saksham-Nautiyal <saksham.nautiyal@dell.com>
2025-08-18 12:54:49 +0530

    added validation for controller_sensors

---
+++ tests/integration/targets/idrac_system_info/tests/_get_controller_sensor_details.py
@@ -0,0 +1,20 @@
+import sys
+import json
+
+controller_sensor_api_output = sys.argv[1]
+NA = "Not Available"
+
+
+def get_controller_data(resp):
+    output = {
+        "FQDD": resp.get("@odata.id", "").rstrip("/").rsplit("/", 1)[-1],
+        "Key": resp.get("@odata.id", "").rstrip("/").rsplit("/", 1)[-1] 
+    }
+    return output
+
+
+output = []
+controller_sensor_output = json.loads(controller_sensor_api_output)
+for each_member in controller_sensor_output.get("Members", []):
+    output.append(get_controller_data(each_member))
+print(output)
