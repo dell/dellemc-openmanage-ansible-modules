114fb58292511d862538c464009f02b1617f4ec7
Saksham-Nautiyal <saksham.nautiyal@dell.com>
2025-08-19 15:36:07 +0530

    version change

--- plugins/module_utils/idrac_utils/info/controller_sensor.py
+++ plugins/module_utils/idrac_utils/info/controller_sensor.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Dell OpenManage Ansible Modules
-# Version 9.13.0
+# Version 10.0.0
 # Copyright (C) 2025 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # Redistribution and use in source and binary forms, with or without modification,
--- tests/integration/targets/idrac_system_info/tests/_get_controller_sensor_details.py
+++ tests/integration/targets/idrac_system_info/tests/_get_controller_sensor_details.py
@@ -5,7 +5,7 @@ controller_sensor_api_output = sys.argv[1]
 NA = "Not Available"
 
 
-def get_controller_data(resp):
+def controller_sensor_mapped_data(resp):
     output = {
         "FQDD": resp.get("@odata.id", "").rstrip("/").rsplit("/", 1)[-1],
         "Key": resp.get("@odata.id", "").rstrip("/").rsplit("/", 1)[-1]
@@ -13,8 +13,14 @@ def get_controller_data(resp):
     return output
 
 
-output = []
-controller_sensor_output = json.loads(controller_sensor_api_output)
-for each_member in controller_sensor_output.get("Members", []):
-    output.append(get_controller_data(each_member))
-print(output)
+def get_controller_sensor_info(api_output):
+    output = []
+    controller_sensor_output = json.loads(api_output)
+    for member in controller_sensor_output.get("Members", []):
+        output.append(controller_sensor_mapped_data(member))
+    return output
+
+
+if __name__ == "__main__":
+    result = get_controller_sensor_info(controller_sensor_api_output)
+    print(json.dumps(result, indent=2))
