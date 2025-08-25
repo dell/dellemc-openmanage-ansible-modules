736d4a825f6ea000cadf6161c6547c1f421ae985
Datta <trisha_datta@dell.com>
2025-08-25 20:29:47 +0530

    Fixing integration and molecule

--- plugins/modules/idrac_redfish_storage_controller.py
+++ plugins/modules/idrac_redfish_storage_controller.py
@@ -3,7 +3,7 @@
 
 #
 # Dell OpenManage Ansible Modules
-# Version 9.11.0
+# Version 10.0.0
 # Copyright (C) 2019-2025 Dell Inc. or its subsidiaries. All Rights Reserved.
 
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
