1520e41fa0d0f5d7fe377aa5227dc12e9a35ba9b
Sakshi_Makkar <Sakshi_Makkar@dell.com>
2025-09-01 04:04:00 -0400

    network_template_vlan UTs fixed

--- tests/unit/plugins/modules/test_ome_template_network_vlan.py
+++ tests/unit/plugins/modules/test_ome_template_network_vlan.py
@@ -314,7 +314,7 @@ class TestOmeTemplateNetworkVlan(FakeAnsibleModule):
         ome_response_mock.json_data = params.get("json_data")
         ome_default_args.update(params.get('mparams'))
         if params.get("fail_json", False):
-            result = self._run_module_with_fail_json(ome_default_args)
+            result = self._run_module(ome_default_args)
         else:
             result = self._run_module(ome_default_args, check_mode=params.get("check_mode", False))
         assert result["msg"] == params['message']
@@ -337,13 +337,13 @@ class TestOmeTemplateNetworkVlan(FakeAnsibleModule):
             mocker.patch(
                 MODULE_PATH + 'validate_vlans',
                 side_effect=exc_type("exception message"))
-            result = self._run_module_with_fail_json(ome_default_args)
+            result = self._run_module(ome_default_args)
             assert result['failed'] is True
         else:
             mocker.patch(MODULE_PATH + 'validate_vlans',
                          side_effect=exc_type('https://testhost.com', 400, 'http error message',
                                               {"accept-type": "application/json"}, StringIO(json_str)))
-            result = self._run_module_with_fail_json(ome_default_args)
+            result = self._run_module(ome_default_args)
             assert result['failed'] is True
         assert 'proxy_configuration' not in result
         assert 'msg' in result
