478d4eb6b56390024155ca8913a1e66f1f751deb
Monika-Illuri <Monika.Illuri@dell.com>
2025-10-26 19:39:14 +0000

    [ECS02A-779] Adding FC modules, tests

--- tests/integration/targets/idrac_system_info/tests/_get_fc_details.py
+++ tests/integration/targets/idrac_system_info/tests/_get_fc_details.py
@@ -5,67 +5,62 @@ fc_api_output = sys.argv[1]
 fc_capability_api_output = sys.argv[2]
 fc_portmetrics_api_output = sys.argv[3]
 fc_statistics_api_output = sys.argv[4]
-ethernet_ethernet_data_api_output = sys.argv[5]
 NA = "Not Available"
 
 
 def get_capability_fc_details(id):
     fc_capability_output = json.loads(fc_capability_api_output)
-    dcb_protocol, fcoe_boot_support, fcoe_offload_support, flex_add_support, \
-        fc_part_support, pxe_boot_support, tcp_chimney_support, wol_support, \
-        iscsi_boot_support, iscsi_offload_support = "", "", "", "", \
-        "", "", "", "", "", ""
+    feature_licensing_support, uefi_support, flex_addressing_support, \
+        on_chip_thermal_sensor, fc_max_num_exchanges, fc_max_num_outstanding_cmds, \
+        persistence_policy_support = "", "", "", "", "", "", ""
     for member in fc_capability_output.get("Members", []):
         if member.get("Id", "") == id:
-            iscsi_offload_support = member.get("iSCSIOffloadSupport", "")
-            dcb_protocol = member.get("DCBExchangeProtocol", "")
-            wol_support = member.get("PartitionWOLSupport", "")
-            fcoe_boot_support = member.get("FCoEBootSupport", "")
-            pxe_boot_support = member.get("PXEBootSupport", "")
-            fcoe_offload_support = member.get("FCoEOffloadSupport", "")
-            flex_add_support = member.get("FlexAddressingSupport", "")
-            iscsi_boot_support = member.get("iSCSIBootSupport", "")
-            fc_part_support = member.get("NicPartitioningSupport", "")
-            tcp_chimney_support = member.get("TCPChimneySupport", "")
-    return dcb_protocol, fcoe_boot_support, fcoe_offload_support, \
-        flex_add_support, fc_part_support, pxe_boot_support, \
-        tcp_chimney_support, wol_support, iscsi_boot_support, \
-        iscsi_offload_support
+            feature_licensing_support = member.get("FeatureLicensingSupport", "")
+            uefi_support = member.get("uEFISupport", "")
+            flex_addressing_support = member.get("FlexAddressingSupport", "")
+            on_chip_thermal_sensor = member.get("OnChipThermalSensor", "")
+            fc_max_num_exchanges = member.get("FCMaxNumberExchanges", "")
+            fc_max_num_outstanding_cmds = member.get("FCMaxNumberOutStandingCommands", "")
+            persistence_policy_support = member.get("PersistencePolicySupport", "")
+    return feature_licensing_support, uefi_support, flex_addressing_support, \
+        on_chip_thermal_sensor, fc_max_num_exchanges, fc_max_num_outstanding_cmds, \
+        persistence_policy_support
 
 
-def get_fc_portmetrics_details(fc_port_id):
+def get_fc_portmetrics_details(id):
     fc_portmetrics_output = json.loads(fc_portmetrics_api_output)
-    link_status = ""
+    os_driver_state, fc_tx_total_frames, fc_rx_total_frames, \
+        fc_tx_sequences, fc_rx_sequences, fc_tx_kb_count, fc_rx_kb_count, \
+        fc_invalid_crcs, fc_loss_of_signals, fc_link_failures, oem_data = \
+        "", "", "", "", "", "", "", "", "", "", "", {}
     for member in fc_portmetrics_output.get("Members", []):
-        if member.get("Id", "") == fc_port_id:
-            link_status = member.get("PartitionLinkStatus", "")
-    return link_status
+        if member.get("Id", "") == id:
+            os_driver_state = member.get("OSDriverState", "")
+            fc_tx_total_frames = member.get("FCTxTotalFrames", "")
+            fc_rx_total_frames = member.get("FCRxTotalFrames", "")
+            fc_tx_sequences = member.get("FCTxSequences", "")
+            fc_rx_sequences = member.get("FCRxSequences", "")
+            fc_tx_kb_count = member.get("FCTxKBCount", "")
+            fc_rx_kb_count = member.get("FCRxKBCount", "")
+            fc_invalid_crcs = member.get("FCInvalidCRCs", "")
+            fc_loss_of_signals = member.get("FCLossOfSignals", "")
+            fc_link_failures = member.get("FCLinkFailures", "")
+
+            oem_data = member.get("Oem", {})
+            if not isinstance(oem_data, dict):
+                oem_data = {}
+    return os_driver_state, fc_tx_total_frames, fc_rx_total_frames, \
+        fc_tx_sequences, fc_rx_sequences, fc_tx_kb_count, fc_rx_kb_count, \
+        fc_invalid_crcs, fc_loss_of_signals, fc_link_failures, oem_data
 
 
-def get_fcstatistics_details(id):
+def get_fc_statistics_details(id):
     fc_statistics_output = json.loads(fc_statistics_api_output)
-    rx_bytes, rx_multicast, rx_unicast, tx_bytes, tx_multicast, \
-        tx_unicast = "", "", "", "", "", ""
+    port_status = ""
     for member in fc_statistics_output.get("Members", []):
         if member.get("Id", "") == id:
-            rx_bytes = member.get("RxBytes", "")
-            tx_unicast = member.get("TxUnicastPackets", "")
-            rx_unicast = member.get("RxUnicastPackets", "")
-            tx_bytes = member.get("TxBytes", "")
-            tx_multicast = member.get("TxMutlicastPackets", "")
-            rx_multicast = member.get("RxMutlicastPackets", "")
-    return rx_bytes, rx_multicast, rx_unicast, tx_bytes, \
-        tx_multicast, tx_unicast
-
-
-def get_fc_ethernet_details():
-    ethernet_ethernet_data_output = json.loads(ethernet_ethernet_data_api_output)
-    mac_address = ethernet_ethernet_data_output.get("MACAddress", "")
-    link_speed = ethernet_ethernet_data_output.get("SpeedMbps", "")
-    auto_neg = ethernet_ethernet_data_output.get("AutoNeg", "")
-    perm_mac_addr = ethernet_ethernet_data_output.get("PermanentMACAddress", "")
-    health = ethernet_ethernet_data_output.get("Status", {}).get("Health", NA)
-    return mac_address, link_speed, auto_neg, perm_mac_addr, health
+            port_status = member.get("PortStatus", "")
+    return port_status
 
 
 def mapped_fc_data(fc, fc_port_id):
@@ -73,68 +68,70 @@ def mapped_fc_data(fc, fc_port_id):
     def sanitize(value):
         return NA if value == "" else value
 
-    dcb_protocol, fcoe_boot_support, fcoe_offload_support, flex_add_support, \
-        fc_part_support, pxe_boot_support, tcp_chimney_support, wol_support, \
-        iscsi_boot_support, iscsi_offload_support = get_capability_fc_details(fc_port_id)
+    feature_licensing_support, uefi_support, flex_addressing_support, \
+        on_chip_thermal_sensor, fc_max_num_exchanges, fc_max_num_outstanding_cmds, \
+        persistence_policy_support = get_capability_fc_details(fc_port_id)
 
-    link_status = get_fc_portmetrics_details(fc_port_id)
-    mac_address, link_speed, auto_neg, perm_mac_addr, health = get_fc_ethernet_details()
-    rx_bytes, rx_multicast, rx_unicast, tx_bytes, tx_multicast, tx_unicast = get_fcstatistics_details(fc_port_id)
+    os_driver_state, fc_tx_total_frames, fc_rx_total_frames, \
+        fc_tx_sequences, fc_rx_sequences, fc_tx_kb_count, fc_rx_kb_count, \
+        fc_invalid_crcs, fc_loss_of_signals, fc_link_failures, oem_data = get_fc_portmetrics_details(fc_port_id)
+
+    port_status = get_fc_statistics_details(fc_port_id)
 
     output = {
-        "iSCSIOffloadSupport": sanitize(iscsi_offload_support),
-        "ControllerBIOSVersion": fc.get("ControllerBIOSVersion", NA),
-        "DCBExchangeProtocol": sanitize(dcb_protocol),
-        "WWN": fc.get("WWN", NA),
-        "DataBusWidth": fc.get("DataBusWidth", NA),
-        "EFIVersion": fc.get("EFIVersion", NA),
-        "FCoEBootSupport": sanitize(fcoe_boot_support),
-        "VirtWWPN": fc.get("VirtWWPN", NA),
-        "FCoEOffloadMode": fc.get("FCoEOffloadMode", NA),
-        "FCoEWWNN": fc.get("FCoEWWNN", NA),
-        "FQDD": fc.get("Id", NA),
-        "FamilyVersion": fc.get("FamilyVersion", NA),
-        "FlexAddressingSupport": sanitize(flex_add_support),
-        "IPv4Address": fc.get("IPv4Addresses", NA),
-        "IPv6Address": fc.get("IPv6Addresses", NA),
-        "Key": fc.get("Id", NA),
-        "LinkDuplex": fc.get("LinkDuplex", NA),
-        "TxBytes": sanitize(tx_bytes),
-        "TxMutlicast": sanitize(tx_multicast),
-        "TxUnicast": sanitize(tx_unicast),
-        "LinkSpeed": sanitize(link_speed),
-        "LinkStatus": sanitize(link_status),
-        "MaxBandwidthPercent": fc.get("MaxBandwidthPercent", NA),
-        "MediaType": fc.get("MediaType", NA),
-        "FCCapabilities": fc.get("FCCapabilities", NA),
-        "NicMode": fc.get("NicMode", NA),
-        "NicPartitioningSupport": sanitize(fc_part_support),
-        "PermanentFCOEMACAddress": fc.get("PermanentFCOEMACAddress", NA),
-        "PXEBootSupport": sanitize(pxe_boot_support),
-        "PrimaryStatus": "Healthy" if health == "OK" else health,
-        "PermanentMACAddress": sanitize(perm_mac_addr),
-        "PermanentiSCSIMACAddress": fc.get("PermanentiSCSIMACAddress", NA),
-        "ProductName": fc.get("ProductName", NA),
-        "Protocol": fc.get("Protocol", NA),
-        "SupportedBootProtocol": fc.get("SupportedBootProtocol", NA),
-        "SwitchConnectionID": fc.get("SwitchConnectionID", NA),
-        "SwitchPortConnectionID": fc.get("SwitchPortConnectionID", NA),
-        "TCPChimneySupport": sanitize(tcp_chimney_support),
-        "VFSRIOVSupport": fc.get("VFSRIOVSupport", NA),
+        "DeviceName": fc.get("DeviceName", NA),
+        "Id": fc.get("Id", NA),
+        "Bus": fc.get("Bus", NA),
+        "PortDownRetryCount": fc.get("PortDownRetryCount", NA),
         "VendorName": fc.get("VendorName", NA),
-        "VirtMacAddr": sanitize(mac_address),
-        "FCoEOffloadSupport": sanitize(fcoe_offload_support),
-        "VirtWWN": fc.get("VirtWWN", NA),
-        "WOLSupport": sanitize(wol_support),
-        "DeviceDescription": fc.get("Description", NA),
-        "WWPN": fc.get("WWPN", NA),
-        "CurrentMACAddress": sanitize(mac_address),
-        "iSCSIBootSupport": sanitize(iscsi_boot_support),
-        "iScsiOffloadMode": fc.get("iScsiOffloadMode", NA),
-        "AutoNegotiation": sanitize(auto_neg),
-        "RxBytes": sanitize(rx_bytes),
-        "RxMutlicast": sanitize(rx_multicast),
-        "RxUnicast": sanitize(rx_unicast)
+        "HardZoneAddress": fc.get("HardZoneAddress", NA),
+        "Description": fc.get("Description", NA),
+        "PortLoginTimeout": fc.get("PortLoginTimeout", NA),
+        "FramePayloadSize": fc.get("FramePayloadSize", NA),
+        "LinkDownTimeout": fc.get("LinkDownTimeout", NA),
+        "SerialNumber": fc.get("SerialNumber", NA),
+        "LoopResetDelay": fc.get("LoopResetDelay", NA),
+        "FCoEOSDriverVersion": fc.get("FCoEOSDriverVersion", NA),
+        "FCTapeEnable": fc.get("FCTapeEnable", NA),
+        "ProductName": fc.get("ProductName", NA),
+        "SecondFCTargetLUN": fc.get("SecondFCTargetLUN", NA),
+        "Name": fc.get("Name", NA),
+        "FabricLoginTimeout": fc.get("FabricLoginTimeout", NA),
+        "ChipType": fc.get("ChipType", NA),
+        "RDMAOSDriverVersion": fc.get("RDMAOSDriverVersion", NA),
+        "HardZoneEnable": fc.get("HardZoneEnable", NA),
+        "EFIVersion": fc.get("EFIVersion", NA),
+        "DeviceDescription": fc.get("DeviceDescription", NA),
+        "FabricLoginRetryCount": fc.get("FabricLoginRetryCount", NA),
+        "ISCSIOSDriverVersion": fc.get("ISCSIOSDriverVersion", NA),
+        "SecondFCTargetWWPN": fc.get("SecondFCTargetWWPN", NA),
+        "Device": fc.get("Device", NA),
+        "LANDriverVersion": fc.get("LANDriverVersion", NA),
+        "FCOSDriverVersion": fc.get("FCOSDriverVersion", NA),
+        "Function": fc.get("Function", NA),
+        "FamilyVersion": fc.get("FamilyVersion", NA),
+        "PortLoginRetryCount": fc.get("PortLoginRetryCount", NA),
+        "PartNumber": fc.get("PartNumber", NA),
+        "PortDownTimeout": fc.get("PortDownTimeout", NA),
+        "FeatureLicensingSupport": sanitize(feature_licensing_support),
+        "UEFISupport": sanitize(uefi_support),
+        "FlexAddressingSupport": sanitize(flex_addressing_support),
+        "OnChipThermalSensor": sanitize(on_chip_thermal_sensor),
+        "FCMaxNumberExchanges": sanitize(fc_max_num_exchanges),
+        "FCMaxNumberOutStandingCommands": sanitize(fc_max_num_outstanding_cmds),
+        "PersistencePolicySupport": sanitize(persistence_policy_support),
+        "FCInvalidCRCs": sanitize(fc_invalid_crcs),
+        "FCLinkFailures": sanitize(fc_link_failures),
+        "FCLossOfSignals": sanitize(fc_loss_of_signals),
+        "FCTxTotalFrames": sanitize(fc_tx_total_frames),
+        "FCRxTotalFrames": sanitize(fc_rx_total_frames),
+        "FCTxSequences": sanitize(fc_tx_sequences),
+        "FCRxSequences": sanitize(fc_rx_sequences),
+        "FCTxKBCount": sanitize(fc_tx_kb_count),
+        "FCRxKBCount": sanitize(fc_rx_kb_count),
+        "OSDriverState": sanitize(os_driver_state),
+        "Oem": oem_data if isinstance(oem_data, dict) else {},
+        "PortStatus": sanitize(port_status)
     }
     return output
 
