66ac8de90205de668e2fd5ee6b94d49f0a2089fd
Monika-Illuri <Monika.Illuri@dell.com>
2025-10-28 11:51:44 +0000

    [ECS02A-779] Adding FC modules, tests

--- tests/integration/targets/idrac_system_info/tests/_get_fc_details.py
+++ tests/integration/targets/idrac_system_info/tests/_get_fc_details.py
@@ -35,7 +35,6 @@ def get_fc_portmetrics_details(id):
         "", "", "", "", "", "", "", "", "", "", {}
     for member in fc_portmetrics_output.get("Members", []):
         if member.get("Id", "") == id:
-            os_driver_state = member.get("OSDriverState", "")
             fc_tx_total_frames = member.get("FCTxTotalFrames", "")
             fc_rx_total_frames = member.get("FCRxTotalFrames", "")
             fc_tx_sequences = member.get("FCTxSequences", "")
@@ -45,6 +44,7 @@ def get_fc_portmetrics_details(id):
             fc_invalid_crcs = member.get("FCInvalidCRCs", "")
             fc_loss_of_signals = member.get("FCLossOfSignals", "")
             fc_link_failures = member.get("FCLinkFailures", "")
+            os_driver_state = member.get("OSDriverState", "")
 
             oem_data = member.get("Oem", {})
             if not isinstance(oem_data, dict):
@@ -79,59 +79,59 @@ def mapped_fc_data(fc, fc_port_id):
     port_status = get_fc_statistics_details(fc_port_id)
 
     output = {
-        "DeviceName": fc.get("DeviceName", NA),
-        "Id": fc.get("Id", NA),
-        "Bus": fc.get("Bus", NA),
-        "PortDownRetryCount": fc.get("PortDownRetryCount", NA),
-        "VendorName": fc.get("VendorName", NA),
-        "HardZoneAddress": fc.get("HardZoneAddress", NA),
-        "Description": fc.get("Description", NA),
-        "PortLoginTimeout": fc.get("PortLoginTimeout", NA),
-        "FramePayloadSize": fc.get("FramePayloadSize", NA),
-        "LinkDownTimeout": fc.get("LinkDownTimeout", NA),
-        "SerialNumber": fc.get("SerialNumber", NA),
-        "LoopResetDelay": fc.get("LoopResetDelay", NA),
-        "FCoEOSDriverVersion": fc.get("FCoEOSDriverVersion", NA),
-        "FCTapeEnable": fc.get("FCTapeEnable", NA),
-        "ProductName": fc.get("ProductName", NA),
-        "SecondFCTargetLUN": fc.get("SecondFCTargetLUN", NA),
-        "Name": fc.get("Name", NA),
-        "FabricLoginTimeout": fc.get("FabricLoginTimeout", NA),
-        "ChipType": fc.get("ChipType", NA),
-        "RDMAOSDriverVersion": fc.get("RDMAOSDriverVersion", NA),
-        "HardZoneEnable": fc.get("HardZoneEnable", NA),
-        "EFIVersion": fc.get("EFIVersion", NA),
-        "DeviceDescription": fc.get("DeviceDescription", NA),
-        "FabricLoginRetryCount": fc.get("FabricLoginRetryCount", NA),
-        "ISCSIOSDriverVersion": fc.get("ISCSIOSDriverVersion", NA),
-        "SecondFCTargetWWPN": fc.get("SecondFCTargetWWPN", NA),
-        "Device": fc.get("Device", NA),
-        "LANDriverVersion": fc.get("LANDriverVersion", NA),
-        "FCOSDriverVersion": fc.get("FCOSDriverVersion", NA),
-        "Function": fc.get("Function", NA),
-        "FamilyVersion": fc.get("FamilyVersion", NA),
-        "PortLoginRetryCount": fc.get("PortLoginRetryCount", NA),
-        "PartNumber": fc.get("PartNumber", NA),
-        "PortDownTimeout": fc.get("PortDownTimeout", NA),
-        "FeatureLicensingSupport": sanitize(feature_licensing_support),
-        "UEFISupport": sanitize(uefi_support),
-        "FlexAddressingSupport": sanitize(flex_addressing_support),
-        "OnChipThermalSensor": sanitize(on_chip_thermal_sensor),
-        "FCMaxNumberExchanges": sanitize(fc_max_num_exchanges),
-        "FCMaxNumberOutStandingCommands": sanitize(fc_max_num_outstanding_cmds),
-        "PersistencePolicySupport": sanitize(persistence_policy_support),
-        "FCInvalidCRCs": sanitize(fc_invalid_crcs),
-        "FCLinkFailures": sanitize(fc_link_failures),
-        "FCLossOfSignals": sanitize(fc_loss_of_signals),
-        "FCTxTotalFrames": sanitize(fc_tx_total_frames),
-        "FCRxTotalFrames": sanitize(fc_rx_total_frames),
-        "FCTxSequences": sanitize(fc_tx_sequences),
-        "FCRxSequences": sanitize(fc_rx_sequences),
-        "FCTxKBCount": sanitize(fc_tx_kb_count),
-        "FCRxKBCount": sanitize(fc_rx_kb_count),
-        "OSDriverState": sanitize(os_driver_state),
+        "PortStatus": sanitize(port_status),
         "Oem": oem_data if isinstance(oem_data, dict) else {},
-        "PortStatus": sanitize(port_status)
+        "OSDriverState": sanitize(os_driver_state),
+        "FCRxKBCount": sanitize(fc_rx_kb_count),
+        "FCTxKBCount": sanitize(fc_tx_kb_count),
+        "FCRxSequences": sanitize(fc_rx_sequences),
+        "FCTxSequences": sanitize(fc_tx_sequences),
+        "FCRxTotalFrames": sanitize(fc_rx_total_frames),
+        "FCTxTotalFrames": sanitize(fc_tx_total_frames),
+        "FCLossOfSignals": sanitize(fc_loss_of_signals),
+        "FCLinkFailures": sanitize(fc_link_failures),
+        "FCInvalidCRCs": sanitize(fc_invalid_crcs),
+        "PersistencePolicySupport": sanitize(persistence_policy_support),
+        "FCMaxNumberOutStandingCommands": sanitize(fc_max_num_outstanding_cmds),
+        "FCMaxNumberExchanges": sanitize(fc_max_num_exchanges),
+        "OnChipThermalSensor": sanitize(on_chip_thermal_sensor),
+        "FlexAddressingSupport": sanitize(flex_addressing_support),
+        "UEFISupport": sanitize(uefi_support),
+        "FeatureLicensingSupport": sanitize(feature_licensing_support),
+        "PortDownTimeout": fc.get("PortDownTimeout", NA),
+        "PartNumber": fc.get("PartNumber", NA),
+        "PortLoginRetryCount": fc.get("PortLoginRetryCount", NA),
+        "FamilyVersion": fc.get("FamilyVersion", NA),
+        "Function": fc.get("Function", NA),
+        "FCOSDriverVersion": fc.get("FCOSDriverVersion", NA),
+        "LANDriverVersion": fc.get("LANDriverVersion", NA),
+        "Device": fc.get("Device", NA),
+        "SecondFCTargetWWPN": fc.get("SecondFCTargetWWPN", NA),
+        "ISCSIOSDriverVersion": fc.get("ISCSIOSDriverVersion", NA),
+        "FabricLoginRetryCount": fc.get("FabricLoginRetryCount", NA),
+        "DeviceDescription": fc.get("DeviceDescription", NA),
+        "EFIVersion": fc.get("EFIVersion", NA),
+        "HardZoneEnable": fc.get("HardZoneEnable", NA),
+        "RDMAOSDriverVersion": fc.get("RDMAOSDriverVersion", NA),
+        "ChipType": fc.get("ChipType", NA),
+        "FabricLoginTimeout": fc.get("FabricLoginTimeout", NA),
+        "Name": fc.get("Name", NA),
+        "SecondFCTargetLUN": fc.get("SecondFCTargetLUN", NA),
+        "ProductName": fc.get("ProductName", NA),
+        "FCTapeEnable": fc.get("FCTapeEnable", NA),
+        "FCoEOSDriverVersion": fc.get("FCoEOSDriverVersion", NA),
+        "LoopResetDelay": fc.get("LoopResetDelay", NA),
+        "SerialNumber": fc.get("SerialNumber", NA),
+        "LinkDownTimeout": fc.get("LinkDownTimeout", NA),
+        "FramePayloadSize": fc.get("FramePayloadSize", NA),
+        "PortLoginTimeout": fc.get("PortLoginTimeout", NA),
+        "Description": fc.get("Description", NA),
+        "HardZoneAddress": fc.get("HardZoneAddress", NA),
+        "VendorName": fc.get("VendorName", NA),
+        "PortDownRetryCount": fc.get("PortDownRetryCount", NA),
+        "Bus": fc.get("Bus", NA),
+        "Id": fc.get("Id", NA),
+        "DeviceName": fc.get("DeviceName", NA)
     }
     return output
 
