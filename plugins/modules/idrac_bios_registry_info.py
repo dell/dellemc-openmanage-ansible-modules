5a49b14226b9d8f1e6cce1bb25b6c713f9ba1d67
sapana05 <sapana.gupta@dell.com>
2026-07-08 02:35:58 -0400

    fix validate-modules error: change author field from list to string

--- plugins/modules/idrac_bios_registry_info.py
+++ plugins/modules/idrac_bios_registry_info.py
@@ -63,8 +63,7 @@ options:
 
 requirements:
     - "python >= 3.9.6"
-author:
-  - "Dell OpenManage Ansible Team"
+author: "Sapana Gupta(@sapana05)"
 notes:
     - "Run this module from a system that has direct access to Dell iDRAC."
     - "This module supports both IPv4 and IPv6 address for I(idrac_ip)."
