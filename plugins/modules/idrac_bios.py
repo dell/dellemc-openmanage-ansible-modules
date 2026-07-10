a272bff7855d3949a6b90fc07c23054f5f19a7ae
Saksham Nautiyal <Saksham.nautiyal@dell.com>
2026-07-10 02:13:09 -0400

    Remove idrac_common.py, revert to dellemc_idrac for auth params
    - Delete idrac_common.py (was a stopgap duplicate of idrac_auth_params)
    - Revert bios.py to collections branch (use DellSoftwareInventory for dynamic FQDD)
    - Switch imports in idrac_bios, idrac_firmware_info, idrac_lifecycle_controller_logs,
      idrac_system_info from idrac_common to dellemc_idrac
    - dellemc_idrac.py keeps iDRACConnection (OMSDK) for deprecated modules

--- plugins/modules/idrac_bios.py
+++ plugins/modules/idrac_bios.py
@@ -321,7 +321,7 @@ import time
 from ansible.module_utils.common.dict_transformations import recursive_diff
 from ansible.module_utils.six.moves.urllib.error import HTTPError, URLError
 from ansible.module_utils.urls import ConnectionError, SSLValidationError
-from ansible_collections.dellemc.openmanage.plugins.module_utils.idrac_common import idrac_auth_params
+from ansible_collections.dellemc.openmanage.plugins.module_utils.dellemc_idrac import idrac_auth_params
 from ansible_collections.dellemc.openmanage.plugins.module_utils.idrac_redfish import iDRACRedfishAPI
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.dellemc.openmanage.plugins.module_utils.utils import idrac_redfish_job_tracking, remove_key, \
